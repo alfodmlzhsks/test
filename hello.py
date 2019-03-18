9442ffdb8ce429ffd52e92c63834da6aafc545a5
Suhy10 <suhy1018@gmail.com>
2019-03-18 20:59:17 +0900

    zxczc1231

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
-print("Hello World!")
+﻿print("Hello World!")
 print("Nice Good!")
 print("zzack")
 print("gugu")
+print("gugu")
+print("gugu")
+print("das2e131")
 print("cointest1 added");
 #github 에서 수정하였습니다~~~~~~~
